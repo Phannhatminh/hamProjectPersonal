138081c713053d0de5db7073a9f5c875f149c1d8
Hoang Trieu <hoangtrieuhuynh22@augustana.edu>
2024-10-06 14:17:41 -0500

    Upload Image for User Avatar

--- HamRadio/src/main/java/edu/augustana/WelcomeScreenController.java
+++ HamRadio/src/main/java/edu/augustana/WelcomeScreenController.java
@@ -2,11 +2,29 @@ package edu.augustana;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+
 
 public class WelcomeScreenController {
+    @FXML
+    private Label appTitleLabel;
 
     @FXML
-    private void switchToSecondary() throws IOException {
+    private Button logInButton;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private Label sloganLabel;
+
+    @FXML
+    private ImageView userAvatar;
+
+    @FXML
+    private void switchToHamUI() throws IOException {
         App.setRoot("HamUI");
     }
 }
